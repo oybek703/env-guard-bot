c62a664053eeb2e458d57b51ec68779af2cbe85d
Yoriqulov Oybek <hhoybek@gmail.com>
2022-12-22 14:09:51 +0500

    db-manager updated

--- src/database/database-manager.ts
+++ src/database/database-manager.ts
@@ -2,8 +2,6 @@ import 'colors'
 import { Sequelize } from 'sequelize'
 import './associations'
 import { sequelize } from './sequelize'
-import { Region } from './models/regions'
-import { District } from './models/districts'
 
 export class DatabaseManager {
   readonly db: Sequelize
@@ -23,21 +21,5 @@ export class DatabaseManager {
         console.error(`Error while connecting to database: ${e.message}`.red.underline)
       }
     }
-  }
-
-  static async getAllRegions() {
-    return await Region.findAll({ raw: true })
-  }
-
-  static async getRegionByName(name: string) {
-    return await Region.findOne({ where: { name }, raw: true })
-  }
-
-  static async getDistrictsByRegion(regionId: number) {
-    return await District.findAll({ where: { RegionId: regionId }, raw: true })
-  }
-
-  static async getDistrictByName(name: string) {
-    return await District.findOne({ where: { name }, raw: true })
   }
 }
